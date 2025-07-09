17747d681a34d65ab2337778ab75d4b37fda2d2b
log002 <118474240+log002@users.noreply.github.com>
2025-07-09 16:29:23 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello")  //we are using node js not browser
+
+console.log(3 
+    +
+     3)  // code readability should be higher, this is not a good practice
+// console.log("Sadek")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+// let state  // state = undefined
+// let state = null  // here state is not undefined, state = null
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => allocated to the undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof"sadek")  // string
+console.log(typeof age)     // object
+console.log(typeof null)    // object
+console.log(typeof undefined)   // undefined
