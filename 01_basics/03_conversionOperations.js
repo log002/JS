729b8881354448a3b4c67c8377fa1cdecccce63c
log002 <118474240+log002@users.noreply.github.com>
2025-07-09 17:22:43 +0000

    conversion operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -26,8 +26,8 @@ let valueInNumber = Number(score)
 let isLoggedIn = 33  // 33 => true
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -38,3 +38,49 @@ let someNumber = 33
 let StringNumber = String(someNumber)
 // console.log(StringNumber)
 // console.log(typeof StringNumber)
+
+
+// ************ Operations *************
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 3)
+// console.log(2 / 3)
+// console.log(2  % 3)
+
+let str1 = "hello"
+let str2 = " sadek"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log("1" + (2 + 2))
+// console.log(2 + 2 + "1")
+// console.log((2 + 2) + "1")
+// console.log((3 + 4) * 5 % 3)
+
+
+// console.log(true)
+// console.log(+true)   // following two are not preferred in production
+// console.log(+"")
+
+let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2 // can be done but not preferred
+
+let gameCounter = 100
+// gameCounter++
+// ++gameCounter
+// console.log(gameCounter);
+let x = gameCounter++
+let y = ++gameCounter
+console.log(x);
+console.log(y);
+
+
