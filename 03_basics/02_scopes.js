ddf22836619864082fb74ff09dcac05179dd119c
log002 <118474240+log002@users.noreply.github.com>
2025-07-10 19:20:39 +0000

    scope level

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,9 +4,52 @@ if(true){
     let a = 10      // block scope or local scope
     const b = 20
     // var c = 30
-    console.log("Inner : ", a)
+    // console.log("Inner : ", a)
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "sadek"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+        
+    }
+    // console.log(website)
+
+    two()
+
+}
+
+// one()
+
+if(true){
+    const username = "sadek"
+    if(username === "sadek"){
+        const website = " youtube"
+        // console.log(username + website)
+    }
+
+    // console.log(website)
+}
+
+// console.log(username)
+
+
+// ********* interesting ***********
+
+console.log(addOne(5))
+function addOne(num){
+    return num + 1
+}
+
+
+
+console.log(addTwo(5))
+const addTwo = function(num){
+    return num + 2
+}
