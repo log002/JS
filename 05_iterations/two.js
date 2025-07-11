0de08a3eb6aade28bab6af1572e4c319ea9fa37d
log002 <118474240+log002@users.noreply.github.com>
2025-07-11 13:24:19 +0000

    while & do-while

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+// while loop
+
+// let i = 0
+// while(i <= 10){
+//     console.log(`Value of i is ${i}`);
+//     i = i + 2
+// }
+
+// let myArray = ['thor', 'ironman', 'flash', 'batman']
+// let arr = 0
+// while(arr < myArray.length){
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+let score = 1
+do {
+    console.log(`Score is ${score}`);
+    score++
+}while(score <= 10);
